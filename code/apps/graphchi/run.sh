490e31c67d512fad0b4ce671f8c27ab54ea3bcd3
shaleen <shaleen@node-0.vtune.lsm-pg0.wisc.cloudlab.us>
2021-05-03 18:08:52 -0500

    graphchi added

---
+++ code/apps/graphchi/run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -x
+DATA=com-orkut.ungraph.txt
+#DATA=com-youtube.ungraph.txt
+INPUT=$SHARED_DATA/$DATA
+APPBASE=$APPBENCH/graphchi/graphchi-cpp/bin/example_apps
+APP=$APPBASE/pagerank
+PARAM=$1
+OUTPUT=$2
+
+FlushDisk()
+{
+	sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"
+	sudo sh -c "sync"
+	sudo sh -c "sync"
+	sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"
+}
+
+cd $APPBENCH/graphchi 
+FlushDisk
+echo "edgelist" | $APPPREFIX $APP file $INPUT niters 8
+set +x
+
