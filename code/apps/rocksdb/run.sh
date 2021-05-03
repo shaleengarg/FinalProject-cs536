e549a089273447a3d1e0825641f748f087b0ef02
shaleen <shaleen@node-0.vtune.lsm-pg0.wisc.cloudlab.us>
2021-05-03 18:44:32 -0500

    rocksdb run.sh

---
+++ code/apps/rocksdb/run.sh
@@ -0,0 +1,3 @@
+THREAD=2
+
+./db_bench --db=./ --value_size=4096 --benchmarks=fillrandom,readrandom,readseq --wal_dir=./WAL_LOG --sync=0 --key_size=100 --write_buffer_size=67108864 --use_existing_db=0 --threads=$THREAD --num=100000
