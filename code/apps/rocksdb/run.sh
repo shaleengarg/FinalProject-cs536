9f45c2d851415cc2560447a96e904401c5d01c62
shaleen <shaleen@node-0.vtune.lsm-pg0.wisc.cloudlab.us>
2021-05-03 18:45:52 -0500

    rm older files

--- code/apps/rocksdb/run.sh
+++ code/apps/rocksdb/run.sh
@@ -1,3 +1,6 @@
 THREAD=2
 
+rm -rf *.sst CURRENT IDENTITY LOCK MANIFEST-* OPTIONS-* WAL_LOG/
+
+
 ./db_bench --db=./ --value_size=4096 --benchmarks=fillrandom,readrandom,readseq --wal_dir=./WAL_LOG --sync=0 --key_size=100 --write_buffer_size=67108864 --use_existing_db=0 --threads=$THREAD --num=100000
